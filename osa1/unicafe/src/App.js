ecd8bd421d9de2ead860051b55b8cf98ca40d080
Shun Kito <shun.kito@aalto.fi>
2021-11-03 13:44:45 +0200

    1.6 done

---
+++ osa1/unicafe/src/App.js
@@ -0,0 +1,33 @@
+import React, { useState } from "react";
+
+const Button = ({ handleClick, text }) => <button onClick={handleClick}> {text} </button>
+
+const Statistic = (props) => (
+  <div>{props.text} {props.count}</div>
+)
+
+const App = () => {
+  //tallenna napit omaan tilaansa
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  const handleGoodClick = () => setGood(good + 1)
+  const handleNeutralClick = () => setNeutral(neutral + 1)
+  const handleBadClick = () => setBad(bad + 1)
+
+  return (
+    <div>
+      <h1>give feedback</h1>
+      <Button handleClick={handleGoodClick} text='good' />
+      <Button handleClick={handleNeutralClick} text='neutral' />
+      <Button handleClick={handleBadClick} text='bad' />
+      <h1>statistics</h1>
+      <Statistic text='good' count={good} />
+      <Statistic text='neutral' count={neutral} />
+      <Statistic text='bad' count={bad} />
+    </div>
+  )
+}
+
+export default App;
